b7983ec6b136232af4029d5008a53aa292006c54
Agnusse <116185310+Agnusse@users.noreply.github.com>
2023-01-11 00:38:54 +0100

    add Script move fan Enemies modified

--- Assets/Scripts/MoveFan.cs
+++ Assets/Scripts/MoveFan.cs
@@ -1,18 +1,54 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 
 public class MoveFan : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Rigidbody _enemyRb;
+    private GameObject _player;
+    // private UpdateScoreTimer _updateScoreTimerScript;
+
+    [SerializeField] float speed;
+
+    
     void Start()
     {
+        _enemyRb = GetComponent<Rigidbody>();
         
+        // make sure to set the tag "Player" on your player character for this to work
+        _player = GameObject.FindWithTag("Player");
+
+        // _updateScoreTimerScript = GameObject.Find("UpdateScoreTimer").GetComponent<UpdateScoreTimer>();
+    }
+    
+    void FixedUpdate()
+    {
+        // move the enemy to the vector position of the player
+        _enemyRb.AddForce((_player.transform.position - transform.position).normalized * speed);
+        // Debug.Log("Player: " + _player.transform.position + "Enemy: " + transform.position);
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void OnCollisionEnter(Collision other)
     {
-        
+        if(other.gameObject.CompareTag("Player"))
+        {
+            // _updateScoreTimerScript.AddEnemiesCounter();
+            Destroy(this.gameObject);
+        }
+    }
+
+
+    // For debugging we can add gizmos to help visualise depth and distance a bit better
+    void OnDrawGizmosSelected()
+    {
+        if (_player != null)
+        {
+            // Draws a blue line from this transform to the target
+            Gizmos.color = Color.blue;
+            Gizmos.DrawLine(transform.position, _player.transform.position);
+        }
     }
 }
--- Assets/Scripts/SpawnFan.cs
+++ Assets/Scripts/SpawnFan.cs
@@ -1,18 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class SpawnFan : MonoBehaviour
 {
+    // Array for enemy prefabs to randomly choose from
+    [SerializeField] GameObject[] Enemies;
+    [SerializeField] float enemySpawnBoundsMax = 50f;
+
+    // Counter for spawned enemies
+    [SerializeField] int spawnedEnemies;
+
+    // This repeat rate is only changeable before starting the scene, to change the repeat rate on runtime we need a different solution
+    [SerializeField] float repeatRateOnStart = 2f;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // With Invoke Repeating only the parameters generated inside the method can be controlled
+        InvokeRepeating("SpawningEnemies", 3f, repeatRateOnStart);
     }
 
-    // Update is called once per frame
-    void Update()
+    private Vector3 GenerateSpawnPosition()
     {
-        
+        // Create a random enemy spawn position 
+        Vector3 enemySpawnPos = new Vector3(Random.Range(-enemySpawnBoundsMax, enemySpawnBoundsMax), 1,
+            Random.Range(-enemySpawnBoundsMax, enemySpawnBoundsMax));
+            
+        // Long form writing for the above Vector3 in line 36. This generates both vectors first and then assembles them
+        // float enemySpawnPosX = Random.Range(-enemySpawnBoundsMax, enemySpawnBoundsMax);
+        // float enemySpawnPosZ = Random.Range(-enemySpawnBoundsMax, enemySpawnBoundsMax);
+        // Vector3 enemySpawnPos = new Vector3(enemySpawnPosX, 0, enemySpawnPosZ);
+
+        return enemySpawnPos;
+    }
+
+    void SpawningEnemies()
+    {
+        // NOTE: Little hack to have a bit more control over the repeatRate, we can bind this to the enemies spawned. We'll change this soon.
+        float repeatRateModifier = Random.Range(0, 10);
+
+        if (repeatRateModifier <= 5)
+        {
+            // Get a random slot from the enemy prefab array
+            int number = Random.Range(0, Enemies.Length);
+
+            // Instantiate a clone from the prefab enemies at the previously generated position
+            Instantiate(Enemies[number], GenerateSpawnPosition(), Enemies[number].transform.rotation);
+
+            spawnedEnemies++;
+        }
+        else
+        {
+            Debug.Log("No enemy for you today!");
+        }
     }
 }
